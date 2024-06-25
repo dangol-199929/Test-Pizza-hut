da574ae098762a4f02728028107f749c504a3d17
Sandeep Dangol <sandeep.dangol@ekbana.net>
2024-06-25 13:41:34 +0545

    update next images

--- next.config.js
+++ next.config.js
@@ -1,45 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: [
-//       "api.uat.ordering-tamarind.ekbana.net",
-//       "uat.ordering-tamarind.ekbana.net",
-//       "system.uat.ordering-tamarind.ekbana.net",
-//       "uat.ordering-iamthegardener-v5.ekbana.net",
-//       "assets.raptor.multiple.ekbana.net",
-//       "api.raptor.multiple.ekbana.net",
-//       "api.uat.ordering-pizzahut.ekbana.net",
-//     ],
-//   },
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/sitemap.xml",
-//         destination: "/api/sitemap",
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  images: {
-    domains: [""],
-  },
   reactStrictMode: true,
   images: {
     domains: [
-      "api.uat.ordering-tamarind.ekbana.net",
-      "uat.ordering-tamarind.ekbana.net",
-      "system.uat.ordering-tamarind.ekbana.net",
       "uat.ordering-iamthegardener-v5.ekbana.net",
       "assets.raptor.multiple.ekbana.net",
       "api.raptor.multiple.ekbana.net",
       "api.uat.ordering-pizzahut.ekbana.net",
+      "assets.raptor.ekbana.net",
+      "assets.uat.ordering-pizzahut.ekbana.net",
+      "https://api.uat.ordering-pizzahut.ekbana.net/",
+      "https://assets.uat.ordering-pizzahut.ekbana.net/",
     ],
   },
 
@@ -54,6 +26,15 @@ const nextConfig = {
     }
     // Important: return the modified config
     return config;
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+      },
+    ];
   },
 };
 
